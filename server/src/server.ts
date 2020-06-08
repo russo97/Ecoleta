a2b2d4a095d2aa482914d614b4ea210fb20638cd
Wanderson Silva <wandersonsilva768@gmail.com>
2020-06-08 05:52:12 -0300

    devolvendo dados na rota /users

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', () => {
+app.get('/users', (request, response) => {
+    response.json([
+        'Wanderson',
+        'Diego'
+    ]);
+
     console.log('listagem de usuários');
 });
 
