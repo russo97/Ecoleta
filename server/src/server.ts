e8d42bdf32a1cf9bba293689ebd392475d56a5bc
Wanderson Silva <wandersonsilva768@gmail.com>
2020-06-09 00:36:29 -0300

    adicionado arquivo separado para lidar com as rotas da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-const users = [
-    'Wanderson',
-    'Diego',
-    'Daniel'
-];
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    return response.json(users[id]);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    const { name, email } = data;
-
-    return response.json({ name, email });
-});
+app.use(routes);
 
 app.listen(3333);
